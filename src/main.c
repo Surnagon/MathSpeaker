8a1bf140b96ad3ea1b73f5da65b954c7abf0236e
seumagal <zr.magal@gmail.com>
2019-12-05 23:43:04 -0800

    [File sacan for one-word numbers]
    >This commits finishes the implementation of the scanning thread,
     which reads from a named pipe the recognized numbers.
    >This commit is the first release of a complete and stable
     num_recog appilicaton

--- src/main.c
+++ src/main.c
@@ -7,32 +7,43 @@ int main()
    bool timeout;
    char charbuffer[124];
    int ret;
+   unsigned * rd;
 
    T_numrecog_cotext context;
    T_numrecog_info buffer[10];
 
-   ret = numrecog_start(&context,10,1,buffer,10,"/home/z/MathSpeaker/recogfile",
+   do
+   {
+      ret = numrecog_start(&context,5000,1000,buffer,10,"recogfile",
                         charbuffer,sizeof(charbuffer));
    if(ret)
    {
       printf("\r\n Erro ao iniciar");
+      return 0;
    }
 
-   if( NULL != numrecog_read(&context,&result,&timeout))
+   timeout =  false;
+   while(!timeout)
    {
-      printf("\r\n o valor reconhecido foi %d", result);
-   }
-   else
-   {
-      if(timeout)
+      rd = numrecog_read(&context,&result,&timeout);
+      if(rd!=NULL)
       {
-         printf("\r\n a crianca  nao respondeu");
+           printf("\r\n o valor reconhecido foi %d", result);
+           printf("\r\n");
+           break;
       }
       else
       {
-          printf("\r\n a crianca nao respondeu ainda");
+         if(timeout)
+         {
+            printf("\r\n a crianca  nao respondeu");
+         }
+         else
+         {
+//                  printf("\r\n a crianca nao respondeu ainda");
+         }
       }
    }
-
+   }while(1);
    return 0;
 }
